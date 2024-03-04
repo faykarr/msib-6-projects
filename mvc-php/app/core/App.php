6f36bdf7a6271b470c49c68ac834762eef825921
faykarr <nasyathfaykar@gmail.com>
2024-03-04 11:40:15 +0700

    mvc-php-feat: initial routing configuration

--- mvc-php/app/core/App.php
+++ mvc-php/app/core/App.php
@@ -3,6 +3,18 @@ class App
 {
     public function __construct()
     {
-        echo "Web MVC Siap Dijalankan!";
+        $url = $this->parseURL();
+        var_dump($url);
+    }
+
+    // Make a method to parse the URL with Filter Sanitize URL
+    public function parseURL()
+    {
+        if (isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
     }
 }
